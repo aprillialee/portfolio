ae8c31239c24a9c84f27ac7d5adc50e41e2809ce
aprillialee <aprilliapmlee@gmail.com>
2021-02-16 14:17:10 +0000

    added menu icons

--- components/nav/MenuOptions.js
+++ components/nav/MenuOptions.js
@@ -7,8 +7,16 @@ function MenuOptions({ open }) {
 const MenuOptionsStyled = styled.ul`
   width: 100px;
   height: 300px;
+  padding-top: 30px;
   background: #200f59;
   transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+  list-style: none;
+`;
+
+const MenuOption = styled.li`
+  width: 100px;
+  margin-bottom: 15px;
+  cursor: pointer;
 `;
 
 export default MenuOptions;
