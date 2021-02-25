b934232aa36ea15854e68d2aa2cbd4c88b1d339e
aprillialee <aprilliapmlee@gmail.com>
2021-02-25 13:47:30 +0000

    removed banner on index page

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,6 @@ export default function Home() {
   return (
     <>
       <HomepageTitle />
-      <ViewOnDesktop>Looks Best On Desktop</ViewOnDesktop>
     </>
   );
 }
-
-const ViewOnDesktop = styled.h1`
-  display: none;
-  @media (max-width: 1040px) {
-    display: flex;
-    position: absolute;
-    top: 50px;
-    left: 50px;
-    color: #b1c8fc;
-    -webkit-text-stroke: 1px white;
-    font-size: 40px;
-    font-family: "Coda Caption", sans-serif;
-    padding-left: 100%;
-    display: inline-block;
-    white-space: nowrap;
-    animation: floatText 25s infinite linear;
-
-    @keyframes floatText {
-      to {
-        transform: translateX(-100%);
-      }
-    }
-  }
-`;
