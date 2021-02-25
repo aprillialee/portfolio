fc378405c344dfe79cd22f58cdf93df65b344b8a
aprillialee <aprilliapmlee@gmail.com>
2021-02-25 16:29:22 +0000

    added spin animation to sphere

--- r3fcomponents/Sphere.js
+++ r3fcomponents/Sphere.js
@@ -2,12 +2,21 @@ import { BackSide } from "three";
 
 import * as THREE from "three";
 
+import { useRef } from "react";
+
 import { useLoader, useFrame } from "react-three-fiber";
 
 function Sphere() {
+  const mesh = useRef();
+
+  useFrame(() => {
+    mesh.current.rotation.x += 0.005;
+  });
+
   const texture = useLoader(THREE.TextureLoader, "/confetti.jpg");
+
   return (
-    <mesh>
+    <mesh ref={mesh}>
       <sphereGeometry attach="geometry" args={[2, 10, 10]} />
       <meshBasicMaterial map={texture} side={BackSide} />
     </mesh>
