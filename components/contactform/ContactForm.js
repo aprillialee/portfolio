5a69876817b91fae802ddc0c959a0390fedbe01a
aprillialee <aprilliapmlee@gmail.com>
2021-02-26 21:00:46 +0000

    finished contact form styling

--- components/contactform/ContactForm.js
+++ components/contactform/ContactForm.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 import { Title, Button } from "../styledcomponents/Styled";
 
+import Link from "next/link";
+
 function ContactForm() {
   return (
     <ContactFormStyled>
@@ -9,13 +11,73 @@ function ContactForm() {
       <ContactAreaContainer>
         <ContactArea>
           <NameArea>
-            <Name />
-            <Name />
+            <Name type="text" name="firstName" placeholder="First Name" />
+            <Name type="text" name="lastName" placeholder="Last Name" />
           </NameArea>
-          <Email />
-          <Message />
+          <Email type="text" name="email" placeholder="Email" />
+          <Message
+            type="text"
+            name="message"
+            placeholder="Enter your message here :)"
+          />
           <Button>Send</Button>
         </ContactArea>
+        <SocialContainer>
+          <Text>
+            Send me a message and say hello <br />
+            or reach out on social media
+          </Text>
+          <IconContainer>
+            <Link href="https://www.instagram.com/aprillialee/" passHref={true}>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 13 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.6"
+                  y="0.6"
+                  width="11.8"
+                  height="11.8"
+                  rx="3.4"
+                  stroke="white"
+                  strokeWidth="1.2"
+                />
+                <circle cx="6.5" cy="6.5" r="3" stroke="white" />
+                <circle cx="9.75" cy="2.75" r="0.75" fill="white" />
+              </svg>
+            </Link>
+            <Link href="https://twitter.com/prillyio" passHref={true}>
+              <svg
+                version="1.1"
+                id="Logo"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 248 204"
+                width="40"
+                height="40"
+                fill="white"
+              >
+                <g id="Logo_1_">
+                  <path
+                    id="white_background"
+                    class="st0"
+                    d="M221.95,51.29c0.15,2.17,0.15,4.34,0.15,6.53c0,66.73-50.8,143.69-143.69,143.69v-0.04
+		C50.97,201.51,24.1,193.65,1,178.83c3.99,0.48,8,0.72,12.02,0.73c22.74,0.02,44.83-7.61,62.72-21.66
+		c-21.61-0.41-40.56-14.5-47.18-35.07c7.57,1.46,15.37,1.16,22.8-0.87C27.8,117.2,10.85,96.5,10.85,72.46c0-0.22,0-0.43,0-0.64
+		c7.02,3.91,14.88,6.08,22.92,6.32C11.58,63.31,4.74,33.79,18.14,10.71c25.64,31.55,63.47,50.73,104.08,52.76
+		c-4.07-17.54,1.49-35.92,14.61-48.25c20.34-19.12,52.33-18.14,71.45,2.19c11.31-2.23,22.15-6.38,32.07-12.26
+		c-3.77,11.69-11.66,21.62-22.2,27.93c10.01-1.18,19.79-3.86,29-7.95C240.37,35.29,231.83,44.14,221.95,51.29z"
+                  />
+                </g>
+              </svg>
+            </Link>
+          </IconContainer>
+        </SocialContainer>
       </ContactAreaContainer>
     </ContactFormStyled>
   );
@@ -32,6 +94,8 @@ const ContactFormStyled = styled.div`
 `;
 
 const ContactAreaContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   width: 95%;
   height: 95%;
 `;
@@ -58,6 +122,15 @@ const Name = styled.input`
   height: 100%;
   background: rgb(255, 255, 255, 0.2);
   border: 1px solid white;
+  padding: 20px;
+  outline: none;
+  font-family: "Coda", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+
+  ::placeholder {
+    color: #e5e5e5;
+  }
 `;
 
 const Email = styled.input`
@@ -65,14 +138,63 @@ const Email = styled.input`
   height: 10%;
   background: rgb(255, 255, 255, 0.2);
   border: 1px solid white;
-  margin: 5px 0px 5px 0px;
+  margin: 10px 0px 10px 0px;
+  outline: none;
+  padding: 20px;
+  font-family: "Coda", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+
+  ::placeholder {
+    color: #e5e5e5;
+  }
 `;
 
-const Message = styled.input`
+const Message = styled.textarea`
   width: 100%;
   height: 60%;
   background: rgb(255, 255, 255, 0.2);
   border: 1px solid white;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
+  outline: none;
+  padding: 20px;
+  font-family: "Coda", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+
+  ::placeholder {
+    color: #e5e5e5;
+  }
 `;
+
+const SocialContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 400px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Text = styled.p`
+  font-family: "Coda", sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+  color: white;
+  text-align: center;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 20%;
+  height: 50px;
+  cursor: pointer;
+`;
+
 export default ContactForm;
